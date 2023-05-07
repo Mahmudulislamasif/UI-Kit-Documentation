4a0acef2fc45028e6f3f9873f47658b74e469866
ASIF\mahmu <Mohammad Mahmudul Islam>
2023-05-07 15:02:24 +0600

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ const toggleMenu = document.querySelector('.toggle-menu');
 		const sidebarContent = document.querySelector('.sidebar-content');
     const sectionContainer=document.querySelectorAll('section');
     const navLinksContainer=document.querySelectorAll('aside nav a')
-		
+		toggleMenu.addEventListener('click', () => {
+			toggleMenu.classList.toggle('active-link');
+			sidebarContent.classList.toggle('active-link');
+		});
     window.onscroll=()=>{
       sectionContainer.forEach(sec=>{
           let top=window.scrollY;
